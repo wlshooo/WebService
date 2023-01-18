a334c3c1d26b57341c0fb40e3132168871192199
wlshooo <kswlsgh@naver.com>
2023-01-18 14:33:28 +0900

    p.118

--- src/main/java/com/jojoldu/book/springboot/domain/posts/PostsService.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/PostsService.java
@@ -1,6 +1,9 @@
 package com.jojoldu.book.springboot.domain.posts;
 
+import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
+import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
 import com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
+import com.jojoldu.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +18,22 @@ public class PostsService {
     public Long save(PostsSaveRequestDto requestDto)
     {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto)
+    {
+        Posts posts = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id=" +id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id)
+    {
+        Posts entity = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id=" +id));
+
+        return new PostsResponseDto(entity);
     }
 }
